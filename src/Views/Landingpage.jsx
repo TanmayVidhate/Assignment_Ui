d5cfcbb2772641f3e8070262070deba0aac2a6c2
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-11-28 00:35:52 +0530

    add code

--- src/Views/Landingpage.jsx
+++ src/Views/Landingpage.jsx
@@ -17,6 +17,29 @@ function Landingpage() {
                     <button className='border-2 border-red-600 rounded-3xl p-2 w-1/3'>Get started</button>
                     <button className='border-2 border-red-600 rounded-3xl p-2 w-1/3'>Learn More</button>
                 </div>
+
+                <div className='w-5/6 mt-10 flex gap-2 m-auto  border-2 border-red-600'>
+                    <div className='w-2/3 p-7 rounded-3xl border-2 border-red-600'>
+                        <div className='w-5/6 border-2 border-red-800'>
+                            <p className='text-5xl'>Organize.</p>
+                            <p className='text-5xl'>Achieve.</p>
+                            <p className='text-5xl'>Relax.</p>
+
+                            <p className='mt-10 text-xl'>Turn clutter into clarity, chaos into control, and dreams into done.bold visions into market success</p>
+
+                            <div className='w-full mt-14  flex items-center justify-start gap-5 border-4 border-red-700'>
+                                <button className='border-2 border-red-600 rounded-3xl p-2 w-1/3'>Get Started Today</button>
+                                <button className='border-2 border-red-600 rounded-3xl p-2 w-1/3'>Discover Features</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className='border-2 border-red-600'>
+                        <p>Your Task.</p>
+                        <p>Your Tools.</p>
+                    </div>
+                </div>
             </div>
         </>
     )
