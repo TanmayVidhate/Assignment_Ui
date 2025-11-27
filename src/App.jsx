cd7508c0866752c39e375c857bee68e520bab6ae
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-11-27 23:51:43 +0530

    add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
 import Navbar from './Components/Navbar.jsx'
 
+import Landingpage from "./Views/Landingpage.jsx"
+
 function App() {
   return (
     <>
       <Navbar/>
+      <Landingpage/>
     </>
   )
 }
