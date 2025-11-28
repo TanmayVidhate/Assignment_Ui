ae48432dae45f003ac7405bbe0c4b96682d0895d
tanmay-vidhate <tanmayvidhate7@gmail.com>
2025-11-28 17:32:34 +0530

    footer done

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+
+import img from '../assets/navbar_logo/nav_logo.png'
+
+function Footer() {
+    return (
+        <>
+            <div className='min-h-min border-2 border-red-800'>
+                <div className='w-[86%]  m-auto flex justify-between mb-5'>
+
+                    {/* first div */}
+                    <div>
+                        {/* logo div */}
+                        <div className='w-1/2 border border-red-500 pl-10'>
+                            <img src={img} alt='logo' className='w-[150px] border border-red-500 ' />
+                        </div>
+
+                        {/* msg */}
+                        <div className='w-full border border-red-500 mt-5'>
+                            <p className='text-lg'>Subscribe to our newsletter for the latest features and updates delivered to you.</p>
+                        </div>
+
+                        {/* input type & button */}
+                        <div className='w-full border border-red-500 mt-5 p-2'>
+                            <input type='text' className='w-1/2 border border-black p-2' placeholder='Your email here' />
+                            <button className='bg-[#FF3E54] text-white w-1/6 ml-2 p-2 rounded-md '>Join</button>
+                            <p className='text-sm mt-2'>By subscribing, you consent to our Privacy Policy and agree to receive updates.</p>
+                        </div>
+                    </div>
+
+                    {/* second div */}
+                    <div className='flex gap-20'>
+
+                        {/* first column links */}
+                        <div>
+                            <p className='font-semibold mb-5'>Useful Links</p>
+                            <ul>
+                                <li>Home Page</li>
+                                <li>About Us</li>
+                                <li>Contact Us</li>
+                                <li>Blog Posts</li>
+                                <li>FAQs</li>
+                            </ul>
+                        </div>
+
+                        {/* second column links */}
+                        <div>
+                            <p className='font-semibold mb-5'>Resources</p>
+                            <ul>
+                                <li>Help Center</li>
+                                <li>User Guide</li>
+                                <li>Community Forum</li>
+                                <li>Feedback</li>
+                                <li>Support</li>
+                            </ul>
+                        </div>
+
+                        {/* third column links */}
+                        <div>
+                            <p className='font-semibold mb-5'>Connect With Us</p>
+                            <ul>
+                                <li>Facebook</li>
+                                <li>Instagram</li>
+                                <li>X</li>
+                                <li>LinkedIn</li>
+                                <li>YouTube</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className='w-[86%] m-auto flex justify-between border-black border-t-2'>
+                    <div className='mt-8'>
+                        <p className=''>© 2024 Osumare. All rights reserved.</p>
+                    </div>
+
+                    <div className='flex mt-8 mb-8 gap-5'>
+                        <p>Privacy Policy</p>
+                        <p>Terms of Service</p>
+                        <p>Cookie Settings</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
